90daa9a05e417622bb74810b132ffacac758df3a
Weber Jardim <jardim.weber@gmail.com>
2016-11-03 20:25:07 -0200

    Atividade Finalizada

--- app.js
+++ app.js
@@ -12,10 +12,31 @@ function getAddress() {
 }
 
 function showLoading() {
-	var $loading = $('.hidden');
-	$loading.addClass('visible');
+	var $loading = $('.loading');
+	$loading.removeClass('hidden');
 }
 
-function requestAddress{
-	
+function hideLoading() {
+	var $loading = $('.loading');
+	$loading.addClass('hidden');
+}
+
+function requestAddress() {
+	var zipcode = $zipcodeInput.val().replace('-','');
+	var url = 'http://192.168.0.54:3000/address/' + zipcode;
+	$.get(url, showAddress);
+}
+
+function showInputs() {
+	$('input.hidden').removeClass('hidden');
+}
+
+function showAddress(response) {
+	hideLoading();
+	$('.street').val(response.street);
+	$('.city').val(response.city);
+	$('.district').val(response.district);
+	$('.state').val(response.state);
+	showInputs();
+	$('.number').focus();
 }
