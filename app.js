6f9b7edd149451d0074b4e9f68eabd56c8f432dc
Gustavo <gustavosantosfe@gmail.com>
2017-10-23 08:46:03 -0200

    replace tab character with 2 spaces
    this is in accordance to google's style guide
    https://google.github.io/styleguide/javascriptguide.xml#JavaScript_Style_Rules

--- app.js
+++ app.js
@@ -4,39 +4,40 @@ $zipcodeInput.mask('00000-000');
 $zipcodeInput.on('keyup', getAddress);
 
 function getAddress() {
-	var isZipCodeValid = $zipcodeInput.val().length === 9;
-	if (isZipCodeValid) {
-		showLoading();
-		requestAddress();
-	}
+  var isZipCodeValid = $zipcodeInput.val().length === 9;
+  if (isZipCodeValid) {
+    showLoading();
+    requestAddress();
+  }
 }
 
 function showLoading() {
-	var $loading = $('.loading');
-	$loading.removeClass('hidden');
+  var $loading = $('.loading');
+  $loading.removeClass('hidden');
 }
 
 function hideLoading() {
-	var $loading = $('.loading');
-	$loading.addClass('hidden');
+  var $loading = $('.loading');
+  $loading.addClass('hidden');
 }
 
 function requestAddress() {
-	var zipcode = $zipcodeInput.val().replace('-','');
-	var url = 'http://192.168.0.54:3000/address/' + zipcode;
-	$.get(url, showAddress);
+  var zipcode = $zipcodeInput.val().replace('-','');
+  var url = 'http://192.168.0.54:3000/address/' + zipcode;
+  $.get(url, showAddress);
 }
 
 function showInputs() {
-	$('input.hidden').removeClass('hidden');
+  $('input.hidden').removeClass('hidden');
 }
 
 function showAddress(response) {
-	hideLoading();
-	$('.street').val(response.street);
-	$('.city').val(response.city);
-	$('.district').val(response.district);
-	$('.state').val(response.state);
-	showInputs();
-	$('.number').focus();
+  hideLoading();
+  $('.street').val(response.street);
+  $('.city').val(response.city);
+  $('.district').val(response.district);
+  $('.state').val(response.state);
+  showInputs();
+  $('.number').focus();
 }
+
